0c5209afd02ab0e53d2eaae1bec613ad025aa9f8
raghavdx007 <raghavdx50@gmail.com>
2021-04-17 18:21:32 +0530

    heavy updates
    update 3.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React,{useState,useEffect} from 'react';
 import './App.css';
 import Post from './post';
-import {db} from './firebase.js';
+import {db , auth} from './firebase.js';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import { Button , Input } from '@material-ui/core'
+import Logo from './assets/images/finstalogo.png';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -21,8 +24,8 @@ function rand() {
   return Math.round(Math.random() * 20) - 10;
 }
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
@@ -34,8 +37,40 @@ function getModalStyle() {
 function App() {
 const[posts,setPosts] = useState([]);
 const[open,setOpen] = useState(false);
+const[username,setUsername] = useState('');
+const[email,setEmail] = useState('');
+const[password,setPassword] = useState('');
 const classes = useStyles();
-const [modalStyle] = React.useState(getModalStyle);
+const [modalStyle] = useState(getModalStyle);
+const[user,setUser] = useState('null');
+
+useEffect(() =>{
+const unsubscribe = auth.onAuthStateChanged((authUser) => {
+  if (authUser) {
+    console.log(authUser);
+    setUser(authUser);
+
+      if (authUser.displayName){
+
+      }
+
+      else
+      {
+        return authUser.updateProfile({
+          displayName:username
+        })
+      }
+  }
+  else {
+    setUser(null);
+  }
+})
+
+return () =>{
+  unsubscribe();
+}
+}, [user,username]);
+
 useEffect (() =>{
 db.collection('posts').onSnapshot(snapshot => {
 setPosts(snapshot.docs.map(doc => ({
@@ -45,7 +80,13 @@ post :doc.data()
 })
   },[]);
 
+const signUp = (event) => {
+event.preventDefault();
 
+auth
+.createUserWithEmailAndPassword(email,password)
+.catch((error) => alert(error.message))
+}
   return (
     <div className="App">
     <Modal
@@ -53,12 +94,38 @@ post :doc.data()
        onClose={() => setOpen(false)}
        >
        <div style={modalStyle} className={classes.paper}>
-       <h2> i am a modal </h2>
-       </div>
-       </Modal>
+         <form className= "app_signup">
+               <center>
+                <img className="app_header_image" src={Logo}></img>
+                </center>
+
+                <Input
+                  type="text"
+                  placeholder="username"
+                  value={username}
+                  onChange={(e) =>setUsername(e.target.value)}
+                  />
+                <Input
+                  placeholder="email"
+                  type="text"
+                  value={email}
+                  onChange={(e) =>setEmail(e.target.value)}
+                  />
+                <Input
+                  placeholder="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) =>setPassword(e.target.value)}
+                />
+
+              <Button type="submit" onClick = {signUp}>SignUp</Button>
+            </form>
+            </div>
+            </Modal>
       <div className="app_header">
-      <img className="app_header_image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQ4YYo7FWpeEEdGAWaMKSKClw3qvIM488bw&usqp=CAU"></img>
+      <img className="app_header_image" src={Logo}></img>
       </div>
+      <button onClick={() => setOpen(true)}>Sign Up</button>
       <h1>Hello world </h1>
       {
         posts.map(({id,post})  =>(
