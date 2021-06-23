d7b10259fd5ec063e18335a77cdd13a027aef245
raghavdx007 <raghavdx50@gmail.com>
2021-06-23 19:25:45 +0530

    firebase new

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@
 import firebase from 'firebase';
 
 const fb = firebase.initializeApp({
-apiKey: "AIzaSyBqbexWmYK6LCQEiC95hlzlDG4GGcQREWg",
-authDomain: "insta-clone-5d07d.firebaseapp.com",
-projectId: "insta-clone-5d07d",
-storageBucket: "insta-clone-5d07d.appspot.com",
-messagingSenderId: "154478588222",
-appId: "1:154478588222:web:15be6831def0e639dbfc6f",
-measurementId: "G-CBTDL1GR7B"
+  apiKey: "AIzaSyC5-IujiyWZWkFksNfdllOqg5jHTvUSQEE",
+  authDomain: "finstagram-01.firebaseapp.com",
+  projectId: "finstagram-01",
+  storageBucket: "finstagram-01.appspot.com",
+  messagingSenderId: "984085177289",
+  appId: "1:984085177289:web:063afac0bf7062de7f3e18",
+  measurementId: "G-2STDME7XRG"
 });
-
+  
 
 export const db = fb.firestore();
 export const auth = fb.auth();
